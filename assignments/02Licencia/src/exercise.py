af39d0150f59196a703eaa49f70ab9314837f705
Arturo Paez <A01284988@itesm.mx>
2021-08-31 09:38:38 -0500

    02Licencia

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,18 @@
 
+from _typeshed import IdentityFunction
+
+
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if edad >= 18:
+        Identificacion = input("¿Tienes Identificación?: ")
+        if Identificacion == "si":
+            print("Trámite de licencia concedido")
+        elif Identificacion == "No":
+            print("No cumples requisitos")
+    else:
+        print("No cumples requisitos")
 
 if __name__ == '__main__':
     main()
