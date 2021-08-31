25bb16ef8b6db292b1739d7e4113d2173d44fffe
Arturo Paez <A01284988@itesm.mx>
2021-08-31 09:47:34 -0500

    03TipoTriangulo

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -4,6 +4,16 @@ def main():
     lado3 = int(input("Ingresa la medida del lado 3: "))
     #Escribe aquí tu código...
 
+    if ((lado1 + lado2)>lado3) and ((lado3 + lado2)>lado3) and ((lado1 + lado3)>lado3):
+        if lado1 == lado2 == lado3:
+            print('Es un Triangulo Equilatero')
+        elif lado1 == lado2 or lado3 == lado2 or lado1 == lado3:
+            print('Es un Triangulo Isosceles')
+        else:
+            print('Es un Escaleno')
+    else:
+        print('No es Triangulo')
+
 
 if __name__=='__main__':
     main()
